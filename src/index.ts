a452268c42813bdd83865883bd71d564a4dc94ec
shafayetejaman <shafayetejaman@gmail.com>
2025-04-30 11:04:04 +0600

    init config completed

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express";
+import morgan from "morgan";
+import logger from "./logger";
+
+dotenv.config();
+const app: Express = express();
+const morganFormat: string = ":method :url :status :response-time ms";
+
+// Custom stream type
+interface LogObject
+{
+  method: string;
+  url: string;
+  status: string;
+  responseTime: string;
+}
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message: string): void =>
+      {
+        const status: string[] = message.split(' ')
+        const logObject: LogObject = {
+          method: status[0],
+          url: status[1],
+          status: status[2],
+          responseTime: status[3] + "ms",
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
+app.use(express.json());
+
+app.get("/notes", (request: Request, response: Response): void =>
+{
+  // Placeholder for fetching notes logic
+  const notes: { id: number, content: string }[] = [
+    { id: 1, content: "Note 1" },
+    { id: 2, content: "Note 2" },
+  ];
+  response.json(notes);
+});
+
+const PORT: number = parseInt(process.env.PORT as string) || 8000;
+
+app.listen(PORT, (): void =>
+{
+  console.log(`Server is running on port ${PORT}`);
+});
