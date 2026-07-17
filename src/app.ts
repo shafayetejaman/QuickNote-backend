b566362a67f2ece7a9eb36aeb99f8c0f126ae7d3
shafayetejaman <shafaitzaman@gmail.com>
2026-07-17 16:58:21 +0600

    fix

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,7 @@ app.use(cookieParser())
 app.use(dbMiddleware)
 app.use(
     cors({
-        origin: process.env.BACKEND_URL,
+        origin: process.env.FRONTEND_URL,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true, // allow cookies
