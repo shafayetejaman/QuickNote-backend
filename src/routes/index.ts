2418aaa18f75e27cfa641e225c264ceafd9e5fc0
shafayetejaman <shafaitzaman@gmail.com>
2026-07-25 11:37:47 +0600

    fix

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,10 @@
 import { Router } from "express"
-import categoryRouter from "./category.router"
-import colorRouter from "./color.router"
 import noteRouter from "./notes.router"
-import tagRouter from "./tag.router"
 import userRouter from "./user.router"
 
 const router = Router()
 
 router.use("/v1/users", userRouter)
 router.use("/v1/notes", noteRouter)
-router.use("/v1/colors", colorRouter)
-router.use("/v1/tags", tagRouter)
-router.use("/v1/categories", categoryRouter)
 
 export default router
